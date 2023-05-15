df6826fe5703399e7a469a3b1f6ce68ddbd6c366
Jessica Kaur Chawla <118368557+jessicachawla@users.noreply.github.com>
2023-05-15 23:16:17 +0530

    2

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -10,3 +10,21 @@ with open(inputFile, "r") as file:
         if input_line == '':
             continue
         ls_inputs.append(input_line)
+
+################################################
+
+declaredLabels_idx = []
+declaredLabels = []
+lblInstruction = []
+tmp_idx = 0
+for line in ls_inputs:
+    tokens = line.strip().split()
+    
+    if tokens and tokens[0].endswith(":"):
+        label_name = tokens[0][:-1]
+        declaredLabels.append(label_name)
+        declaredLabels_idx.append((label_name, tmp_idx))
+        
+        if len(tokens) > 1: lblInstruction.append(tokens[1:])
+    tmp_idx += 1
+
