cb4b6f650a611eaafc59c013f3643a1b1979c7e6
Arnav <118423878+Arnavg24@users.noreply.github.com>
2023-05-15 23:18:01 +0530

    3

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -27,4 +27,31 @@ for line in ls_inputs:
         
         if len(tokens) > 1: lblInstruction.append(tokens[1:])
     tmp_idx += 1
+    
+################################################
+
+calledLabels_idx = []
+calledLabels2 = []
+calledVars = []
+calledVars2 = []
+
+tmp_idx = 0
+for tmp_idx, line in enumerate(ls_inputs):
+    tokens = line.strip().split()
+    
+    if tokens and tokens[0].endswith(":") and len(tokens) > 1:
+        if tokens[1] in ['ld', 'st']:
+            calledVars.append(tokens[-1])
+            calledVars2.append([tokens[-1], tmp_idx])
+        elif tokens[1] in ['jmp', 'jlt', 'jgt', 'je']:
+            calledLabels_idx.append(tokens[-1])
+            calledLabels2.append([tokens[-1], tmp_idx])
+    
+    elif tokens and tokens[0] in ['ld', 'st']:
+        calledVars.append(tokens[-1])
+        calledVars2.append([tokens[-1], tmp_idx])
+    elif tokens and tokens[0] in ['jmp', 'jlt', 'jgt', 'je']:
+        calledLabels_idx.append(tokens[-1])
+        calledLabels2.append([tokens[-1], tmp_idx])
+
 
