07c4c884f6a2d403d60154e1ee8edcf9f2d678aa
Akshat22055 <131397257+Akshat22055@users.noreply.github.com>
2023-05-15 23:34:15 +0530

    2

--- assembler/ISA.py
+++ assembler/ISA.py
@@ -38,3 +38,67 @@ opcodeTable = {
     'je': ['11111', 'E'],
     'hlt': ['11010', 'F']
 }
+type_to_reg_no = {
+    'A': 3,
+    'B': 1,
+    'C': 2,
+    'D': 1,
+    'E': 0,
+    'F': 0
+}
+
+type_to_input_len = {
+    'A': 4,
+    'B': 3,
+    'C': 3,
+    'D': 3,
+    'E': 2,
+    'F': 1
+}
+
+type_to_imm_no = {
+    'A': 0,
+    'B': 1,
+    'C': 0,
+    'D': 0,
+    'E': 0,
+    'F': 0
+}
+
+type_to_unusedbits = {
+    'A': 2,
+    'B': 1,
+    'C': 5,
+    'D': 1,
+    'E': 4,
+    'F': 11
+}
+
+type_to_memoryaddress = {
+    'A': 0,
+    'B': 0,
+    'C': 0,
+    'D': 1,
+    'E': 1,
+    'F': 0
+}
+
+register_to_encoding = {
+    "R0": "000",
+    "R1": "001",
+    "R2": "010",
+    "R3": "011",
+    "R4": "100",
+    "R5": "101",
+    "R6": "110",
+    "FLAGS": "111",
+}
+
+type_to_members = {
+    "A": ["Instruction", "Register", "Register", "Register"],
+    "B": ["Instruction", "Register", "Immediate"],
+    "C": ["Instruction", "Register", "Register"],
+    "D": ["Instruction", "Register", "Memory Address"],
+    "E": ["Instruction", "Memory Address"],
+    "F": ["Instruction"]
+}
